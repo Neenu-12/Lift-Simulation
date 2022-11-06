944ee7655da6645b6250bc9491ce96105a92d3d7
Neenu Kumar <neenukumar24@gmail.com>
2022-11-06 18:20:18 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,48 @@
 const form = document.querySelector('form');
-// const div = document.querySelector('div');
+let fnum = 0;
+const destination_record = []
 
 form.addEventListener('submit',function(e){
   e.preventDefault();
-  const lift_num = document.getElementById('lift_num').value;
-  const floor_num = document.getElementById('floor_num').value;
-  console.log(lift_num);
-  console.log(floor_num);
+  document.getElementById('submit').disabled = true;
+  let lift_num = document.getElementById('lift_num').value;
+  let floor_num = document.getElementById('floor_num').value;
 
-  const generator = () => {
+  for(let i = 0; i < floor_num; i++){
+    generator(lift_num,floor_num);
+    // console.log(i);
+  }
+  lift_appender(lift_num);
+});
+
+function generator(lift_num,floor_num){
   const lift_area = document.querySelector('.lift-area');
   const buttons_n_lift = document.createElement('div'); 
   buttons_n_lift.setAttribute('class','buttons-n-lift');
 
   const button_div = document.createElement('div');
-  button_div.setAttribute('class','button_div');
-
-  const button_center_div = document.createElement('div');
-  button_center_div.setAttribute('class','button-center-div');
+  button_div.setAttribute('class','button-div');
     
   const button_up = document.createElement('button');
   button_up.setAttribute('class','up');
+  button_up.innerText = "Up";
+  button_up.setAttribute('data-floor',fnum);
+  button_up.addEventListener('click',() => lift_move(button_up.getAttribute('data-floor')));
+  button_up.addEventListener('click',lift_move(fnum));
+  if(fnum === floor_num -1){
+    button_up.style.display = 'none';
+  }
+
+  const fitText = require("FitText-UMD");
 
   const button_down = document.createElement('button');
   button_down.setAttribute('class','down');
+  button_down.innerText = "Down";
+  button_down.setAttribute('data-floor',fnum);
+  button_down.addEventListener('click',() => lift_move(button_up.getAttribute('data-floor')));
+  if(fnum === 0){
+    button_down.style.display = 'none';
+  }
 
   const lift_div = document.createElement('div');
   lift_div.setAttribute('class','lift-div');
@@ -40,101 +59,59 @@ form.addEventListener('submit',function(e){
   floor_right_div.setAttribute('class','right');
 
   const floor_p = document.createElement('p');
+  floor_p.innerText = `Floor ${fnum}`;
 
   floor_left_div.append(hr);
   floor_right_div.append(floor_p);
   floor_big_div.append(floor_left_div,floor_right_div);
-  button_center_div.append(button_up,button_down);
-  button_div.append(button_center_div);
-
-  // lift.append(liftt);
-
-  for(let i = 0; i < lift_num; i++){
-    const lift = document.createElement('div');
-    lift.setAttribute('class','lift');
-    lift_div.append(lift);
-    if(i > 0){
-      lift.style.marginLeft = "5%";
-    }
-  }
+  button_div.append(button_up,button_down);
 
   buttons_n_lift.append(button_div,lift_div);
-  lift_area.append(buttons_n_lift,floor_big_div);
+  lift_area.prepend(buttons_n_lift,floor_big_div);
+  fnum++;
 }
 
-for(let i = floor_num; i > 0; i--){
-  generator();
-
-  if(i !== 1){
-    const extra_lifts = document.querySelectorAll('.lift');
-    for(let x = 0; x < lift_num; x++){
-      extra_lifts[x].remove();
+function lift_appender(lift_num){
+  const lift_divs = document.querySelectorAll('.lift-div');
+  for(i = 0; i < lift_num; i++){
+    const lift = document.createElement('div');
+    lift.setAttribute('class','lift');
+    lift.setAttribute('data-status','free');
+    lift.setAttribute('data-initial-position',0);
+    destination_record.push('0');
+    lift_divs[lift_divs.length - 1].append(lift);
+    if(i > 0){
+      lift.style.marginLeft = "4%";
     }
   }
 }
 
-for(let j = floor_num; j > 0; j--){
-  const up_text = document.querySelectorAll('.up');
-  up_text[j-1].innerText = "Up";
-  const down_text = document.querySelectorAll('.down');
-  down_text[j-1].innerText = "Down";
-  const p_text = document.querySelectorAll('p');
-  p_text[j-1].innerText = `Floor ${floor_num - j + 1}`;
-  // p_text[j-1].innerText = `Floor ${floor_num - (j + 1)}`;  // doubt
-  // console.log(j);
-  // console.log(p_text[j-1]);
-}
-
-const lift = document.querySelectorAll('.lift');
-
-// // Moving lifts up :-
-// const up = document.querySelectorAll('.up');
-// console.log(up);
-// for(let i = 0; i < up.length; i++){
-//     up[i].addEventListener('click',function(){
-//         lift.style.transform = "translateY(-150%)";
-//         lift.style.transitionDuration = "2s";
-//         console.log("asd");
-//     })
-// }
-
-// // Moving lifts down :-
-// const down = document.querySelectorAll('.down');
-// console.log(down);
-// for(let i = 0; i < down.length; i++){
-//     down[i].addEventListener('click',function(){
-//         lift.style.transform = "translateY(137%)";
-//         lift.style.transitionDuration = "2s";
-//         // console.log("defs");
-//     })
-// }
-
-// Moving the lifts to a particular floor :-
-
-for(let l = 0; l < lift_num; l++){
-
-  // Moving up :-
-  const up = document.querySelectorAll('.up');
-  for(let i = 0; i < floor_num; i++){
-      up[i].addEventListener('click',function(){
-          const s = (floor_num - i -1) * (-139);
-          lift[l].style.transform = `translateY(${s}%)`;
-          lift[l].style.transitionDuration = "2s";
-          console.log("asd");
-      })
-  }
-
-  // Moving down :-
-
-  const down = document.querySelectorAll('.down');
-  for(let i = 0; i < floor_num; i++){
-      down[i].addEventListener('click',function(){
-          const s = (floor_num - i -1) * (-137);
-          lift[l].style.transform = `translateY(${s}%)`;
-          lift[l].style.transitionDuration = "2s";
-          console.log("def");
-      })
+function lift_move(destination){
+  console.log(destination);
+  console.log(destination_record);
+  const lift_list = document.querySelectorAll('.lift');
+  for(let l = 0; l < lift_list.length; l++){
+      if(lift_list[l].getAttribute('data-initial-position') != destination){
+        if(lift_list[l].getAttribute('data-status') != 'busy'){
+          mover(destination,l);
+          break;
+        }
+      }
   }
 }
 
-});
+function mover(destination,pos){
+      const lift_list = document.querySelectorAll('.lift');
+      if(!(destination_record.includes(destination))){
+        lift_list[pos].setAttribute('data-status','busy');
+        const movement = (destination) * (-133);
+        lift_list[pos].style.transform = `translateY(${movement}%)`;
+        lift_list[pos].style.transitionDuration = "5s";
+        setTimeout(() => {
+          lift_list[pos].setAttribute('data-status','free');
+        },5000);
+        lift_list[pos].setAttribute('data-initial-position',destination);
+        destination_record[pos] = destination;
+    }
+}
+
